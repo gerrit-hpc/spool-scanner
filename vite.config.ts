91b32481593e1aeb2a325aeb54e2c209ceb64c34
Gerrit Alves <gerrit@omoqo.com>
2026-02-07 10:28:35 +0100

    feat: Add GitHub Actions workflow for deploying to GitHub Pages and update base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react()],
-    base: "/",
+    base: "/spool-scanner/",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
